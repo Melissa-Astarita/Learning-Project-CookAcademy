8b3a1483fb32276c7f31d4a4078dc3088af9512d
Melissa Astarita <122825214+Melissa-Astarita@users.noreply.github.com>
2024-03-11 16:11:46 +0100

    Add all files and folder

---
+++ CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Melissa Astarita on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
